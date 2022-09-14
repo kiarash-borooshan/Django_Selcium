df61e11207b72c7d3ce20e254d328ea215d99140
kiarash.borooshan <kiarash.borooshan@gmail.com>
2022-09-14 19:16:05 +0430

    Article model done and requirements.txt created

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,5 +1,20 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
+
 class Article(models.Model):
+    Title = models.CharField(max_length=200)
+    Slug = models.SlugField(max_length=100, unique=True)
+    Description = models.TextField()
+    Thumbnail = models.ImageField(upload_to="blog/images")
+    Publish = models.DateTimeField(default=timezone.now)
+    Created = models.DateTimeField(auto_now_add=True)
+    Updated = models.DateTimeField(auto_now=True)
+    Status_choices = [
+        {"Draft", "d"},
+        {"Published", "p"}
+    ]
+    Status = models.CharField(max_length=10,
+                              choices=Status_choices,
+                              default="d")
